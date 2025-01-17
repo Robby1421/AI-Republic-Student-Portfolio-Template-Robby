0215e5a36b7044f80cd027f3d74b9d03956d7875
robby1421 <robby.pombo@gmail.com>
2025-01-17 17:06:34 +0800

    update title parcel rescue

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ const projects = [
     link : "https://github.com/Robby1421/RecommenderSystem"
   },
   {
-    title: "PARCEL RESCUE",
+    title: "Parcel Rescue AI",
     image: "https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?w=500&fit=crop",
     problem: "Damaged parcels during transit are a common issue in the logistics and e-commerce industries, leading to customer dissatisfaction, financial losses, and reputational damage. Without an effective system for identifying and managing damaged packages, businesses struggle to quickly address these issues and minimize their impact.",
     solution: "Parcel Rescue uses AI-driven technology to identify and assess damaged parcels in real-time, providing a streamlined process for reporting, tracking, and managing damaged goods. This ensures quicker resolution, reduces customer complaints, and helps companies maintain high standards of service by minimizing losses from damaged shipments.",
